57bb997461c904b2294792e6ac954cffb1697820
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-03 23:40:20 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,11 @@
 def find_min_value(array)
   array.sort.first
 end
+
+def find_min_value(array)
+  counter = 0 
+  min_value = -1
+  while counter < array.length 
+  min_value < array[counter]
+  counter += 1 
+  end
